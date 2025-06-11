b5141fecf24cebc0e341df50886976026cb094c0
cdeddie <edwinchendesailly@gmail.com>
2025-06-11 14:02:34 +0930

    Correct gh pages action yaml

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,11 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue()],
+export default defineConfig(({ command }) => {
+  const isProduction = command === 'build'
+  
+  return {
+    plugins: [vue()],
+    base: isProduction ? '/creative-display/' : '/',
+  }
 })
